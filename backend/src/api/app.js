2f51771c5f450a998974a99ec37f3c1ee141cdd1
thiagopcdev <thiagopc_dev@hotmail.com>
2022-06-19 12:28:49 -0300

    Adiciona Cors ao backend

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,9 +1,16 @@
+const cors = require('cors');
 const express = require('express');
 const { articleRouter, categoryRouter } = require('../router');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: ['POST', 'GET', 'PUT', 'DELETE'],
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use('/articles', articleRouter);
 app.use('/categories', categoryRouter);
 app.use(require('../middleware/error'));
