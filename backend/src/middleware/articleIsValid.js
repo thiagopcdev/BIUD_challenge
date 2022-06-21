1ba38967a2c9d81d0fc9912fe235f77daa2a5058
thiagopcdev <thiagopc_dev@hotmail.com>
2022-06-21 16:43:06 -0300

    Adiciona valor minimo de entrada do titulo e descricao

--- backend/src/middleware/articleIsValid.js
+++ backend/src/middleware/articleIsValid.js
@@ -7,6 +7,10 @@ module.exports = async (req, res, next) => {
       return res.status(StatusCodes.BAD_REQUEST)
         .json({ message: 'Invalid entries. Try again.' });
     }
+    if (title.length < 3) {
+      return res.status(StatusCodes.BAD_REQUEST)
+        .json({ message: 'Title must be at least 3 characters long' });
+    }
     next();
   } catch (e) {
     next(e);
